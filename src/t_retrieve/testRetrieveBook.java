6e5bdfceb02775dc8a77e82871eac2562caad571
lucas <lucas@DESKTOP-43O43ND>
2017-05-28 18:52:34 -0300

    Testes de pesquisa criados para Books e Movies, mas precisar ser conferidos!

--- src/t_retrieve/testRetrieveBook.java
+++ src/t_retrieve/testRetrieveBook.java
@@ -5,16 +5,31 @@
  */
 package t_retrieve;
 
+import Conn.Connection;
+import DBManager.BooksDBM;
+import entities.Books;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
+import t_files.OpenTestFiles;
 
 /**
  *
  * @author lucas
  */
 public class testRetrieveBook extends AbstractJavaSamplerClient implements Serializable {
+
+    OpenTestFiles openTestFiles = new OpenTestFiles();
+    BooksDBM booksDBM = new BooksDBM();
+    
+    private List<Books> books = new ArrayList<>();
+    private List<Object> retrievedBooks = new ArrayList<>();
+    List<String[]> inserts = new ArrayList<>();
+
+    private final String testSize = "10";
 
     @Override
     public SampleResult runTest(JavaSamplerContext jsc) {
@@ -23,13 +38,26 @@ public class testRetrieveBook extends AbstractJavaSamplerClient implements Seria
         return result;
     }
 
-    public void retrieveBookByTitleOrAuthorOrSeriesOrPublisherOrISBN() {
-        //isbn
-        //title
-        //author
-        //serie
-        //publisher
-
+    //isbn
+    //title
+    //author
+    //serie
+    //publisher
+    public void testMethod() {
+        SampleResult result = new SampleResult();
+        inserts = openTestFiles.open("retrieve\\retrieveBooks", testSize);
+        
+        result.sampleStart();
+        Connection.getCon().getTransaction().begin();
+        inserts.forEach((iterator) -> {
+            Object book;
+            //verificar a ordem salva no vetor com a ordem dos parametros do retrieveBooks
+            book = booksDBM.retrieveBookByMultipleValues(iterator[0], iterator[1], iterator[2], iterator[3], iterator[4]);
+            retrievedBooks.add(book);
+        });
+        Connection.getCon().getTransaction().commit();
+        result.sampleEnd();
+        System.out.println("\n\nTempo de execução do teste: " + result.getTime());
+        result.setSuccessful(true);
     }
-
 }
--- src/t_retrieve/testRetrieveMovie.java
+++ src/t_retrieve/testRetrieveMovie.java
@@ -5,10 +5,16 @@
  */
 package t_retrieve;
 
+import Conn.Connection;
+import DBManager.MoviesDBM;
+import entities.Movies;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
+import t_files.OpenTestFiles;
 
 /**
  *
@@ -16,19 +22,46 @@ import org.apache.jmeter.samplers.SampleResult;
  */
 public class testRetrieveMovie extends AbstractJavaSamplerClient implements Serializable {
 
+    OpenTestFiles openTestFiles = new OpenTestFiles();
+    MoviesDBM moviesDBM = new MoviesDBM();
+
+    private List<Movies> movies = new ArrayList<>();
+    private List<Object> retrievedMovies = new ArrayList<>();
+    List<String[]> inserts = new ArrayList<>();
+
+    private final String testSize = "10";
+
+    @Override
     public SampleResult runTest(JavaSamplerContext jsc) {
         SampleResult result = new SampleResult();
 
         return result;
     }
 
+    //title
+    //director
+    //writer
+    //actor
+    //network
+    //book
     public void testMethod() {
-        //title
-        //director
-        //writer
-        //actor
-        //network
-        //book
+        SampleResult result = new SampleResult();
+        inserts = openTestFiles.open("retrieve\\retrieveMovies", testSize);
+        
+                result.sampleStart();
+        Connection.getCon().getTransaction().begin();
+        inserts.forEach((iterator) -> {
+            Object movie;
+            //verificar a ordem salva no vetor com a ordem dos parametros do retrieveMovies
+            movie = moviesDBM.retrieveMovieByMultipleValues(iterator[0], iterator[1], iterator[2], iterator[3], iterator[4], iterator[5]);
+            retrievedMovies.add(movie);
+        });
+        Connection.getCon().getTransaction().commit();
+        result.sampleEnd();
+        System.out.println("\n\nTempo de execução do teste: " + result.getTime());
+        result.setSuccessful(true);
+        
+        
     }
 
 }
