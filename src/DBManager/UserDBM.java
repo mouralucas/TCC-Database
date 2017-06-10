7e8e50fa4810d4ee295ff6baf218182cd842efd2
TCC2 <TCC2@192.168.1.72>
2017-06-10 18:42:24 -0300

    Jmeter funcionando em mongoDb e mysql ok , falta corrigir insert aquivo

--- src/DBManager/UserDBM.java
+++ src/DBManager/UserDBM.java
@@ -1,65 +1,65 @@
-package DBManager;
-
-/**
- * @author Jociane Franzoni de Lima
- * @author Lucas Penha de Moura
- *
- * ------------------- Trabalho de Conclusão de Curso ---------------------
- * ---------------------- Engenharia de Computação ------------------------
- * ------------- Universidade Tecnológica Federal do Paraná ---------------
- *
- */
-
-
-import Conn.Connection;
-import entities.Users;
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-
-public class UserDBM {
-
-//    private EntityManagerFactory factory = Persistence
-//            .createEntityManagerFactory("NewLibraryWeb_PU");
-//    private EntityManager em = factory.createEntityManager();
-    
-//    boolean con = Connection.setCon();
-    Connection con = new Connection();
-    EntityManager em = con.getCon(); 
-    
-
-    public Users getUser(String userName, String password) {
-        
-        
-        try {
-            Users usuario = (Users) em
-                    .createQuery(
-                            "SELECT u from Users u where u.nomeUsuario = :name and u.senha = :senha")
-                    .setParameter("name", userName)
-                    .setParameter("senha", password).getSingleResult();
-
-            return usuario;
-        } catch (NoResultException e) {
-            return null;
-        }
-    }
-
-    public boolean insertUser(Users user) {
-        try {
-            em.persist(user);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public boolean removeUser(Users user) {
-        try {
-            em.remove(user);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-}
+//package DBManager;
+//
+///**
+// * @author Jociane Franzoni de Lima
+// * @author Lucas Penha de Moura
+// *
+// * ------------------- Trabalho de Conclusão de Curso ---------------------
+// * ---------------------- Engenharia de Computação ------------------------
+// * ------------- Universidade Tecnológica Federal do Paraná ---------------
+// *
+// */
+//
+//
+//import Conn.Connection;
+//import entities.Users;
+//import javax.persistence.EntityManager;
+//import javax.persistence.NoResultException;
+//
+//public class UserDBM {
+//
+////    private EntityManagerFactory factory = Persistence
+////            .createEntityManagerFactory("NewLibraryWeb_PU");
+////    private EntityManager em = factory.createEntityManager();
+//    
+////    boolean con = Connection.setCon();
+//    Connection con = new Connection();
+//    EntityManager em = con.getCon(); 
+//    
+//
+//    public Users getUser(String userName, String password) {
+//        
+//        
+//        try {
+//            Users usuario = (Users) em
+//                    .createQuery(
+//                            "SELECT u from Users u where u.nomeUsuario = :name and u.senha = :senha")
+//                    .setParameter("name", userName)
+//                    .setParameter("senha", password).getSingleResult();
+//
+//            return usuario;
+//        } catch (NoResultException e) {
+//            return null;
+//        }
+//    }
+//
+//    public boolean insertUser(Users user) {
+//        try {
+//            em.persist(user);
+//            return true;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+//
+//    public boolean removeUser(Users user) {
+//        try {
+//            em.remove(user);
+//            return true;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+//}
--- src/entities/Users.java
+++ src/entities/Users.java
@@ -1,61 +1,61 @@
-package entities;
-
-/**
- * @author Jociane Franzoni de Lima
- * @author Lucas Penha de Moura
- *
- * ------------------- Trabalho de Conclusão de Curso ---------------------
- * ---------------------- Engenharia de Computação ------------------------
- * ------------- Universidade Tecnológica Federal do Paraná ---------------
- *
- */
-
-import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-@Entity
-public class Users implements Serializable {
-
-    @Id
-    @Column(name = "id", nullable = false, unique = true)
-    private int id;
-
-    @Column(name = "userName", nullable = false, unique = true)
-    private String nomeUsuario;
-
-    @Column(name = "password", nullable = false, unique = false)
-    private String senha;
-
-    @Column(name = "lastAccess", unique = true)
-    @Temporal(TemporalType.DATE)
-    private Date ultimoAcesso;
-
-    public String getNomeUsuario() {
-        return nomeUsuario;
-    }
-
-    public void setNomeUsuario(String nomeUsuario) {
-        this.nomeUsuario = nomeUsuario;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public Date getUltimoAcesso() {
-        return ultimoAcesso;
-    }
-
-    public void setUltimoAcesso(Date ultimoAcesso) {
-        this.ultimoAcesso = ultimoAcesso;
-    }
-}
+//package entities;
+//
+///**
+// * @author Jociane Franzoni de Lima
+// * @author Lucas Penha de Moura
+// *
+// * ------------------- Trabalho de Conclusão de Curso ---------------------
+// * ---------------------- Engenharia de Computação ------------------------
+// * ------------- Universidade Tecnológica Federal do Paraná ---------------
+// *
+// */
+//
+//import java.io.Serializable;
+//import java.util.Date;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Temporal;
+//import javax.persistence.TemporalType;
+//
+//@Entity
+//public class Users implements Serializable {
+//
+//    @Id
+//    @Column(name = "id", nullable = false, unique = true)
+//    private int id;
+//
+//    @Column(name = "userName", nullable = false, unique = true)
+//    private String nomeUsuario;
+//
+//    @Column(name = "password", nullable = false, unique = false)
+//    private String senha;
+//
+//    @Column(name = "lastAccess", unique = true)
+//    @Temporal(TemporalType.DATE)
+//    private Date ultimoAcesso;
+//
+//    public String getNomeUsuario() {
+//        return nomeUsuario;
+//    }
+//
+//    public void setNomeUsuario(String nomeUsuario) {
+//        this.nomeUsuario = nomeUsuario;
+//    }
+//
+//    public String getSenha() {
+//        return senha;
+//    }
+//
+//    public void setSenha(String senha) {
+//        this.senha = senha;
+//    }
+//
+//    public Date getUltimoAcesso() {
+//        return ultimoAcesso;
+//    }
+//
+//    public void setUltimoAcesso(Date ultimoAcesso) {
+//        this.ultimoAcesso = ultimoAcesso;
+//    }
+//}
